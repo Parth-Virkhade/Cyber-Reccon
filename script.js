d5e65e5fe997a21230960e91250af2ec82660ccd
Parth-Virkhade <parthvirkhade@gmail.com>
2024-04-23 01:14:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('search-submit').addEventListener('click', function() {
+    const searchInput = document.getElementById('search-input').value;
+    if (searchInput) {
+        window.location.href = 'lorem-ipsum.html';
+    }
+});
+
+const generatePdfButton = document.getElementById('generate-pdf');
+
+generatePdfButton.addEventListener('click', () => {
+  const pdfContent = document.getElementById('pdf-content').innerHTML;
+
+  const opt = {
+    margin: 1,
+    filename: 'lorem-ipsum.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+  };
+
+  html2pdf().from(pdfContent).set(opt).save();
+});
